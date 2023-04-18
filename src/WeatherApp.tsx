8159c6016bd3d6395301a8f07a2789f3b44d75a8
Bram <bramtech@telenet.be>
2023-04-18 09:04:10 +0200

    Allow multiple app states

--- src/WeatherApp.tsx
+++ src/WeatherApp.tsx
@@ -3,12 +3,16 @@ import { Component, useEffect, useState } from "react";
 import { EmptyQuery, searchWeather, WeatherQuery, WeatherResponse } from "./api/WeatherApi";
 
 import "./WeatherApp.css";
-import {WeatherForm} from "./WeatherForm";
+import { WeatherForm } from "./WeatherForm";
 
-// TODO: Implement settings menu
+enum AppState {
+    Main,
+    Settings,
+}
 
 const WeatherApp = () => {
     const [response, setResponse] = useState<WeatherResponse>(WeatherResponse.Nothing);
+    const [state, setState] = useState<AppState>(AppState.Main);
 
     async function onFormReceived(query?: WeatherQuery) {
         if (!query) return;
@@ -26,30 +30,37 @@ const WeatherApp = () => {
         searchWeather(query).then((r) => setResponse(r));
     }, []);
 
-    return (
-        <div className="weather">
-            <WeatherForm onFormSubmit={onFormReceived}></WeatherForm>
-            {response.match({
-                Info: (resp: CurrentResponse) => (
-                    <main>
-                        <header>
-                            <h2>Showing weather info for {resp.name}</h2>
-                        </header>
-                        <div className="today">
-                            <p>Work in progress if you couldn't tell.</p>
-                            <p>{resp.main.temp}</p>
-                        </div>
-                    </main>
-                ),
-                Error: (msg: string) => (
-                    <div>
-                        <p style={{ color: "hotpink" }}>{msg}</p>
-                    </div>
-                ),
-                Nothing: () => <div></div>,
-            })}
-        </div>
-    );
+    switch (state) {
+        case AppState.Main:
+            return (
+                <div className="weather">
+                    <WeatherForm onFormSubmit={onFormReceived}></WeatherForm>
+                    {response.match({
+                        Info: (resp: CurrentResponse) => (
+                            <main>
+                                <header>
+                                    <h2>Showing weather info for {resp.name}</h2>
+                                </header>
+                                <div className="today">
+                                    <p>Work in progress if you couldn't tell.</p>
+                                    <p>{resp.main.temp}</p>
+                                </div>
+                            </main>
+                        ),
+                        Error: (msg: string) => (
+                            <div>
+                                <p style={{ color: "hotpink" }}>{msg}</p>
+                            </div>
+                        ),
+                        Nothing: () => <div></div>,
+                    })}
+                </div>
+            );
+        case AppState.Settings:
+            return <div>Settings</div>;
+        default:
+            throw new Error("Unknown AppState!");
+    }
 };
 
 const App = () => {
