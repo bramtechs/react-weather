f7bd20085385bfe069d7ad491be2e1047b76d8de
bramtechs <bramtech@telenet.be>
2023-05-03 19:56:23 +0200

    Tweak menu bar css

--- src/MenuBar.tsx
+++ src/MenuBar.tsx
@@ -1,33 +1,21 @@
 import { AppState, AppStates } from "./App";
-// TODO: only use fluentui
-import { StyledIcon } from "@styled-icons/styled-icon";
 import { ReactElement } from "react";
-import { StyledIconBase } from "@styled-icons/styled-icon";
-import styled from "styled-components";
-import { Settings } from "styled-icons/fluentui-system-filled";
-import { Home } from "styled-icons/entypo";
+import { HomeFilled, SettingsFilled } from "@fluentui/react-icons";
+import { FluentIcon } from "@fluentui/react-icons/lib/utils/createFluentIcon";
 
 type StateIcons = {
-    [key in AppState]: ReactElement<StyledIcon>;
+    [key in AppState]: ReactElement<FluentIcon>;
 };
-
-export const IconStyleWrapper = styled.div`
-    ${StyledIconBase} {
-        color: red;
-    }
-`;
 
 const _icons: StateIcons = {
-    Main: <Home />,
-    Settings: <Settings />,
+    Main: <HomeFilled />,
+    Settings: <SettingsFilled />,
 };
 
-// TODO: Move tailwind css to index.css
-
-const MenuIcon = (props: { tooltip: string; icon: ReactElement<StyledIcon> }) => {
+const MenuIcon = (props: { tooltip: string; icon: ReactElement<FluentIcon> }) => {
     return (
         <li className="group menu-icon">
-            <div className="m-1 invert">{props.icon}</div>
+            <div className="mb-1 invert">{props.icon}</div>
             <span className="group-hover:scale-100 menu-tooltip">{props.tooltip}</span>
         </li>
     );
@@ -35,7 +23,7 @@ const MenuIcon = (props: { tooltip: string; icon: ReactElement<StyledIcon> }) =>
 
 export const MenuBar = (props: { curState: AppState }) => {
     return (
-        <ul className="fixed top-0 left-0 h-screen w-14 flex flex-col bg-blue-900">
+        <ul className="menu-bar">
             {AppStates.map((state) => (
                 <MenuIcon tooltip={state} key={state} icon={_icons[state]} />
             ))}
