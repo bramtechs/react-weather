f50c81449e2f8a1238355998cd97754770d94ff1
Bram <bramtech@telenet.be>
2023-05-05 11:45:16 +0200

    Pop-in animation for tiles!

--- src/comps/tile/impl/TileContainer.tsx
+++ src/comps/tile/impl/TileContainer.tsx
@@ -1,4 +1,4 @@
-import {ReactElement} from "react";
+import {ReactElement, useEffect, useState} from "react";
 
 export enum TileBackground {
     Unknown,
@@ -7,15 +7,30 @@ export enum TileBackground {
 }
 
 const Gradients = {
-    [TileBackground.Sunny]: `tile-container from-sky-400 to-sky-300`,
-    [TileBackground.Clouds]: `tile-container from-gray-400 to-gray-300`,
-    [TileBackground.Unknown]: `tile-container from-gray-300 to-gray-200`,
+    [TileBackground.Sunny]: [
+        `tile-container scale-0 from-sky-400 to-sky-300`,
+        `tile-container scale-1 from-sky-400 to-sky-300`,
+    ],
+    [TileBackground.Clouds]: [
+        `tile-container scale-0 from-gray-400 to-gray-300`,
+        `tile-container scale-1 from-gray-400 to-gray-300`
+    ],
+    [TileBackground.Unknown]: [
+        `tile-container scale-0 from-gray-300 to-gray-200`,
+        `tile-container scale-1 from-gray-300 to-gray-200`
+    ],
 };
 
 // Background graphic to put weather info in
 export const TileContainer = (props: { children: ReactElement[], type: TileBackground}) => {
+    const [started, setStarted] = useState<boolean>(false);
+
+    useEffect(() => {
+        setStarted(true);
+    },[])
+
     return (
-        <div className={Gradients[props.type]}>
+        <div className={Gradients[props.type][started?1:0]}>
             {props.children}
         </div>
     );
