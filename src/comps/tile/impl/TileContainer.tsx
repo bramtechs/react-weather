5d827302ea4d1d07921948cc686ca680b10f9755
Bram <bramtech@telenet.be>
2023-05-05 20:56:53 +0200

    Make background blurry

--- src/comps/tile/impl/TileContainer.tsx
+++ src/comps/tile/impl/TileContainer.tsx
@@ -1,4 +1,4 @@
-import {ReactElement, useEffect, useState} from "react";
+import { ReactElement, useEffect, useState } from 'react';
 
 export enum TileBackground {
     Unknown,
@@ -8,34 +8,23 @@ export enum TileBackground {
 }
 
 const Gradients = {
-    [TileBackground.Sunny]: [
-        `tile-container scale-0 bg-[url('/tilesbg/sunshine.jpg')]`,
-        `tile-container scale-1 bg-[url('/tilesbg/sunshine.jpg')]`,
-    ],
-    [TileBackground.Clouds]: [
-        `tile-container scale-0 bg-[url('/tilesbg/clouds.jpg')]`,
-        `tile-container scale-1 bg-[url('/tilesbg/clouds.jpg')]`
-    ],
-    [TileBackground.Rain]: [
-        `tile-container scale-0 bg-[url('/tilesbg/rain.jpg')]`,
-        `tile-container scale-1 bg-[url('/tilesbg/rain.jpg')]`
-    ],
-    [TileBackground.Unknown]: [
-        `tile-container scale-0 from-gray-700 to-gray-400`,
-        `tile-container scale-1 from-gray-700 to-gray-400`
-    ],
+    [TileBackground.Sunny]: `tile-background bg-[url('/tilesbg/sunshine.jpg')]`,
+    [TileBackground.Clouds]: `tile-background bg-[url('/tilesbg/clouds.jpg')]`,
+    [TileBackground.Rain]: `tile-background bg-[url('/tilesbg/rain.jpg')]`,
+    [TileBackground.Unknown]: `tile-background from-gray-700 to-gray-400`,
 };
 
 // Background graphic to put weather info in
-export const TileContainer = (props: { children: ReactElement | ReactElement[], type: TileBackground}) => {
+export const TileContainer = (props: { children: ReactElement | ReactElement[]; type: TileBackground }) => {
     const [started, setStarted] = useState<boolean>(false);
 
     useEffect(() => {
         setStarted(true);
-    },[])
+    }, []);
 
     return (
-        <div className={Gradients[props.type][started?1:0]}>
+        <div className={started ? 'relative tile-container scale-1' : 'relative tile-container scale-0'}>
+            <div className={Gradients[props.type]}></div>
             {props.children}
         </div>
     );
