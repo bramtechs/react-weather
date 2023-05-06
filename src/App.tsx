26dcd2f2adf648c0a09e20b6f45d0d7b4bb353b3
bramtechs <bramtech@telenet.be>
2023-05-06 13:39:13 +0200

    Use typescript for main react file

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,15 @@ import { useState } from "react";
 
 import { SettingsState } from "./states/SettingsState";
 import { MenuBar } from "./comps/MenuBar";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { MainState } from "./states/MainState";
 
 export type AppState = "Main" | "Settings";
 export const AppStates: AppState[] = ["Main", "Settings"];
 
-const queryClient = new QueryClient();
-
 const App = () => {
     const [state, setState] = useState<AppState>("Main");
 
     return (
-        <QueryClientProvider client={queryClient}>
             <main className="main-content">
                 <MenuBar curState={state} onStateChange={setState}/>
                 {state === "Main" && (
@@ -29,8 +24,6 @@ const App = () => {
                     ></SettingsState>
                 )}
             </main>
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
     );
 };
 
