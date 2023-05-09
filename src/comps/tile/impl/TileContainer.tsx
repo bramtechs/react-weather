0517a13b016b9ae00154d6623604a262a67be982
bramtechs <bramtech@telenet.be>
2023-05-09 20:09:50 +0200

    do not tile background image of tiles

--- src/comps/tile/impl/TileContainer.tsx
+++ src/comps/tile/impl/TileContainer.tsx
@@ -9,10 +9,10 @@ export enum TileBackground {
 }
 
 const Gradients = {
-    [TileBackground.Sunny]: `absolute left-0 top-0 w-full h-full -z-10 rounded-2xl bg-slate-600 blur-sm bg-[url('/tilesbg/sunshine.jpg')]`,
-    [TileBackground.Clouds]: `absolute left-0 top-0 w-full h-full -z-10 rounded-2xl bg-slate-600 blur-sm bg-[url('/tilesbg/clouds.jpg')]`,
-    [TileBackground.Rain]: `absolute left-0 top-0 w-full h-full -z-10 rounded-2xl bg-slate-600 blur-sm bg-[url('/tilesbg/rain.jpg')]`,
-    [TileBackground.Unknown]: `absolute left-0 top-0 w-full h-full -z-10 rounded-2xl bg-slate-600 blur-sm from-gray-700 to-gray-400`,
+    [TileBackground.Sunny]: `  absolute left-0 top-0 w-full h-full -z-10 rounded-2xl bg-slate-600 blur-sm bg-cover bg-[url('/tilesbg/sunshine.jpg')]`,
+    [TileBackground.Clouds]: ` absolute left-0 top-0 w-full h-full -z-10 rounded-2xl bg-slate-600 blur-sm bg-cover bg-[url('/tilesbg/clouds.jpg')]`,
+    [TileBackground.Rain]: `   absolute left-0 top-0 w-full h-full -z-10 rounded-2xl bg-slate-600 blur-sm bg-cover bg-[url('/tilesbg/rain.jpg')]`,
+    [TileBackground.Unknown]: `absolute left-0 top-0 w-full h-full -z-10 rounded-2xl bg-slate-600 blur-sm bg-cover from-gray-700 to-gray-400`,
 };
 
 // Background graphic to put weather info in
@@ -26,7 +26,7 @@ export const TileContainer = (props: { children: ReactElement | ReactElement[]; 
     const startedCss =
         'relative flex justify-center border-slate-500/80 border-4 border-solid items-center flex-grow m-2 min-w-[40%] h-[20rem] text-center rounded-2xl bg-gradient-to-t text-white dark:text-black font-bold transition-all scale-1';
     const stoppedCss =
-        'relative flex justify-center border-slate-500/0 border-4 border-solid items-center flex-grow m-2 min-w-[40%] h-[20rem] text-center rounded-2xl bg-gradient-to-t text-white dark:text-black font-bold transition-all scale-0';
+        'relative flex justify-center border-slate-500/0  border-4 border-solid items-center flex-grow m-2 min-w-[40%] h-[20rem] text-center rounded-2xl bg-gradient-to-t text-white dark:text-black font-bold transition-all scale-0';
 
     return (
         <div className={started ? startedCss : stoppedCss}>
