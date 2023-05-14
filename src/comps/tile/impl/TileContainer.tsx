cde717d9b33fd7079ddef0fc7fb52926591ab7a3
bramtechs <bramtech@telenet.be>
2023-05-14 18:35:38 +0200

    prevent selection

--- src/comps/tile/impl/TileContainer.tsx
+++ src/comps/tile/impl/TileContainer.tsx
@@ -24,9 +24,9 @@ export const TileContainer = (props: { children: ReactElement | ReactElement[]; 
     }, []);
 
     const startedCss =
-        'group relative flex justify-center border-slate-500/80 border-4 border-solid items-center m-2 min-w-[40rem] h-[20rem] text-center rounded-2xl bg-gradient-to-t text-white dark:text-black font-bold transition-all scale-1';
+        'group relative select-none flex justify-center border-slate-500/80 border-4 border-solid items-center m-2 min-w-[40rem] h-[20rem] text-center rounded-2xl bg-gradient-to-t text-white dark:text-black font-bold transition-all scale-1';
     const stoppedCss =
-        'group relative flex justify-center border-slate-500/0  border-4 border-solid items-center m-2 min-w-[40rem] h-[20rem] text-center rounded-2xl bg-gradient-to-t text-white dark:text-black font-bold transition-all scale-0';
+        'group relative select-none flex justify-center border-slate-500/0  border-4 border-solid items-center m-2 min-w-[40rem] h-[20rem] text-center rounded-2xl bg-gradient-to-t text-white dark:text-black font-bold transition-all scale-0';
 
     return (
         <div className={started ? startedCss : stoppedCss}>
