c1a04a115209bbe90c4e2614a1e431b394689358
bramtechs <bramtech@telenet.be>
2023-05-17 17:42:08 +0200

    color picker todo in settings state

---
+++ src/comps/widgets/FormColorpicker.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function FormColorpicker(props: { name: string; value: string; onChange: (color: string) => void }) {
+    return (
+        <label className="block m-3">
+            {props.name}
+            TODO PUT COLORPICKER HERE!
+        </label>
+    );
+}
--- src/states/SettingsState.tsx
+++ src/states/SettingsState.tsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import { FormDropdown } from "../comps/widgets/FormDropdown";
 import { TempUnit, TempUnits, UserSettings } from '../storage/SettingsAbstractor';
 import { StyledButton } from '../comps/StyledButton';
+import { FormColorpicker } from '../comps/widgets/FormColorpicker';
 import React from 'react';
 
 export const SettingsState = (props: { onFormSubmit: () => void }) => {
     const [unit, setUnit] = useState<TempUnit>(UserSettings().unit);
+    const [themeColor, setThemeColor] = useState<string>("#fffff");
 
     function sendForm() {
         UserSettings((s) => {
@@ -22,6 +24,7 @@ export const SettingsState = (props: { onFormSubmit: () => void }) => {
     return (
         <div className="text-center">
             <FormDropdown name="Temperature Unit" value={unit} options={TempUnits} onChange={(u) => setUnit(u as TempUnit)}/>
+            <FormColorpicker name="Sidebar color" value={themeColor} onChange={setThemeColor}/>
             <div className="flex w-full flex-grow justify-evenly">
                 <StyledButton onClick={wipeTiles} theme='Red'>Reset tiles</StyledButton>
                 <StyledButton onClick={sendForm}>Confirm</StyledButton>
