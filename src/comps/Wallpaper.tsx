a120e5d8e21028ea72648c6631635ec10fb497c0
bramtechs <bramtech@telenet.be>
2023-05-20 18:58:06 +0200

    fix background

--- src/comps/Wallpaper.tsx
+++ src/comps/Wallpaper.tsx
@@ -4,7 +4,7 @@ import { BackgroundTheme, BackgroundThemeCSS } from '../gfx/BackgroundThemes';
 export function Wallpaper(props: { theme: BackgroundTheme }) {
     // TODO: this is scuffed
     return (
-        <div className="absolute -left-[5%] -top-[5%] w-[105%] h-[105%] overflow-hidden -z-20">
+        <div className="fixed -left-[5%] -top-[5%] w-[105%] h-[105%] overflow-hidden -z-20">
             <div className={BackgroundThemeCSS[props.theme]}></div>
         </div>
     );
